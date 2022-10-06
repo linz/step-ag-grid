922690b60b6efa1d20d86ca896da1d8aa2032ced
Matt Dawson <mdawson@linz.govt.nz>
2022-10-07 09:49:17 +1300

    Fix select header updating

--- web/src/components/GenericCellClass.tsx
+++ web/src/components/GenericCellClass.tsx
@@ -25,11 +25,3 @@ export const GenericMultiEditCellClass: CellClassFunc = (props): string => {
     ? "ag-selected-for-edit"
     : "";
 };
-
-/**
- * Applies the ag-selected-for-edit class not only to the cells "being edited" as understood by ag-grid, which is the
- * cell the user right-clicked on, but also to any other cell from a selected row in the same column.
- *
- * This does not get called on the cell that is being edited, it seems.
- */
-export const GenericCellClass = GenericMultiEditCellClass;
