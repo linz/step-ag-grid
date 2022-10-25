22f138c997a711c678eba43ffd67cad84e2f4c55
Matt Dawson <mdawson@linz.govt.nz>
2022-10-25 16:21:03 +1300

    Fix build

---
+++ web/config-overrides.js
@@ -0,0 +1,17 @@
+module.exports = function override(config, env) {
+  let loaders = config.resolve;
+  loaders.fallback = {
+    fs: false,
+    tls: false,
+    net: false,
+    http: require.resolve("stream-http"),
+    https: false,
+    zlib: require.resolve("browserify-zlib"),
+    path: require.resolve("path-browserify"),
+    stream: require.resolve("stream-browserify"),
+    util: require.resolve("util/"),
+    crypto: require.resolve("crypto-browserify"),
+  };
+
+  return config;
+};
