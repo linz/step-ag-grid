6c2e9064b1cf9c1b5bc7c0b13f08af0ddc8715cb
Matt Dawson <mdawson@linz.govt.nz>
2022-10-25 17:09:38 +1300

    I dont know what Im doing

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -1,44 +1,41 @@
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const path = require("path");
-
 module.exports = {
-    babel: async (options) => ({
-        // Update your babel configuration here
-        ...options,
-    }),
-    framework: '@storybook/react',
-    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-    ],
-    webpackFinal: async (config) => {
-        // Handling scss files when used within components consumed by a story
-        config.module.rules.push({
-            test: /\.scss$/,
-            use: ["style-loader", "css-loader", "sass-loader"],
-            include: path.resolve(__dirname, "../"),
-        });
+  babel: async options => ({
+    // Update your babel configuration here
+    ...options
+  }),
+  framework: '@storybook/react',
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [],
+  webpackFinal: async config => {
+    // Handling scss files when used within components consumed by a story
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../")
+    });
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [["react-app", {
+          flow: false,
+          typescript: true
+        }], ["@babel/preset-react", {
+          runtime: "automatic"
+        }]]
+      }
+    });
+    // Resolving the paths for dynamic locations (@components -> ./xxx/xxx/xxx/Components)
+    config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin({
+      extensions: config.resolve.extensions
+    })];
 
-        config.module.rules.push({
-            test: /\.(ts|tsx)$/,
-            loader: require.resolve("babel-loader"),
-            options: {
-                presets: [
-                    ["react-app", { flow: false, typescript: true }],
-                    ["@babel/preset-react", { runtime: "automatic" }],
-                ],
-            },
-        });
-        // Resolving the paths for dynamic locations (@components -> ./xxx/xxx/xxx/Components)
-        config.resolve.plugins = [
-            ...(config.resolve.plugins || []),
-            new TsconfigPathsPlugin({
-                extensions: config.resolve.extensions,
-            }),
-        ];
-
-        // Return the altered config
-        return config;
-    },
+    // Return the altered config
+    return config;
+  },
+  core: {
+    builder: "webpack5"
+  }
 };
--- web/config-overrides.js
+++ web/config-overrides.js
@@ -1,16 +1,7 @@
 module.exports = function override(config, env) {
   let loaders = config.resolve;
   loaders.fallback = {
-    fs: false,
-    tls: false,
-    net: false,
-    http: require.resolve("stream-http"),
-    https: false,
-    zlib: require.resolve("browserify-zlib"),
-    path: require.resolve("path-browserify"),
-    stream: require.resolve("stream-browserify"),
-    util: require.resolve("util/"),
-    crypto: require.resolve("crypto-browserify"),
+    crypto: false,
   };
 
   return config;
