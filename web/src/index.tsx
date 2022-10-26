7231861698cc9f58486a29ed75e4c416b71d31c8
matttdawson <89495499+matttdawson@users.noreply.github.com>
2022-10-26 15:05:36 +1300

    BREAKING CHANGE: Publish module
    * Rollup working
    
    * removed web directory
    
    * Try to fix worklfows.
    
    * Add missing package.json
    
    * Upgrade actions versions
    
    * Downgrade versions

--- web/src/index.tsx
+++
@@ -1,5 +0,0 @@
-export type { GridBaseRow } from "./components/Grid";
-export { Grid } from "./components/Grid";
-export { UpdatingContextProvider } from "./contexts/UpdatingContextProvider";
-export { GridContextProvider } from "./contexts/GridContextProvider";
-export { GridCell } from "./components/GridCell";
