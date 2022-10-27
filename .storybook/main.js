5c504aad6cb1e58d56f60cee98fbbc391a664b7f
Matt Dawson <mdawson@linz.govt.nz>
2022-10-27 15:27:17 +1300

    Try to fix chromatic

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,46 +1,44 @@
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
-  babel: async options => ({
-    // Update your babel configuration here
-    ...options
-  }),
-  framework: '@storybook/react',
+  // Specifying the location of the stories and file formats of what stories can be
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // Specifying the add ons for Storybook if requiring specific add ons to make a story work then add them here
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "storybook-addon-mock/register",
     "@storybook/addon-interactions",
   ],
-  webpackFinal: async config => {
+
+  webpackFinal: async (config) => {
     // Handling scss files when used within components consumed by a story
     config.module.rules.push({
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../")
+      include: path.resolve(__dirname, "../"),
     });
+
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       loader: require.resolve("babel-loader"),
       options: {
-        presets: [["react-app", {
-          flow: false,
-          typescript: true
-        }], ["@babel/preset-react", {
-          runtime: "automatic"
-        }]]
-      }
+        presets: [
+          ["react-app", { flow: false, typescript: true }],
+          ["@babel/preset-react", { runtime: "automatic" }],
+        ],
+      },
     });
     // Resolving the paths for dynamic locations (@components -> ./xxx/xxx/xxx/Components)
-    config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin({
-      extensions: config.resolve.extensions
-    })];
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
 
     // Return the altered config
     return config;
   },
-  core: {
-    builder: "webpack5"
-  }
 };
