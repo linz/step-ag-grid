0a30dabf298a2a61b35e8a434ea96ff41c6af7da
Matt Dawson <mdawson@linz.govt.nz>
2022-10-27 17:24:59 +1300

    Downgrade weblogic4

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-
 module.exports = {
   // Specifying the location of the stories and file formats of what stories can be
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -11,32 +10,29 @@ module.exports = {
     "storybook-addon-mock/register",
     "@storybook/addon-interactions",
   ],
-
-  webpackFinal: async (config) => {
+  webpackFinal: async config => {
     // Handling scss files when used within components consumed by a story
     config.module.rules.push({
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../"),
+      include: path.resolve(__dirname, "../")
     });
-
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       loader: require.resolve("babel-loader"),
       options: {
-        presets: [
-          ["react-app", { flow: false, typescript: true }],
-          ["@babel/preset-react", { runtime: "automatic" }],
-        ],
-      },
+        presets: [["react-app", {
+          flow: false,
+          typescript: true
+        }], ["@babel/preset-react", {
+          runtime: "automatic"
+        }]]
+      }
     });
     // Resolving the paths for dynamic locations (@components -> ./xxx/xxx/xxx/Components)
-    config.resolve.plugins = [
-      ...(config.resolve.plugins || []),
-      new TsconfigPathsPlugin({
-        extensions: config.resolve.extensions,
-      }),
-    ];
+    config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin({
+      extensions: config.resolve.extensions
+    })];
 
     // Return the altered config
     return config;
