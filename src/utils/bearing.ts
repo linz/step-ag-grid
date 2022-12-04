04878d89a92d286375014996f69233f47f3ca750
Ines Falcao <70115704+egeofalcao@users.noreply.github.com>
2022-12-05 09:36:32 +1300

    Fixed bearing correction formatter when type string (#145)

--- src/utils/bearing.ts
+++ src/utils/bearing.ts
@@ -14,7 +14,7 @@ export const bearingCorrectionValueFormatter = (params: ValueFormatterParams): s
     return "–";
   }
   if (typeof value === "string") {
-    return convertDDToDMS(bearingNumberParser(value), true, true);
+    return convertDDToDMS(bearingNumberParser(value), true, false);
   }
   return convertDDToDMS(value, true, false);
 };
