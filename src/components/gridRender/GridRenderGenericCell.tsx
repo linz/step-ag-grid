7fd143a00826060b1048ad9b7e32f738f6ae48b8
Matt Dawson <mdawson@linz.govt.nz>
2022-12-22 11:53:43 +1300

    fix: set row type on editable

--- src/components/gridRender/GridRenderGenericCell.tsx
+++ src/components/gridRender/GridRenderGenericCell.tsx
@@ -1,14 +1,19 @@
 import { ICellRendererParams } from "ag-grid-community";
 import { GridBaseRow } from "../Grid";
 import { ColDefT } from "../GridCell";
-import { SuppressKeyboardEventParams } from "ag-grid-community/dist/lib/entities/colDef";
+import { EditableCallbackParams, SuppressKeyboardEventParams } from "ag-grid-community/dist/lib/entities/colDef";
 
 export interface RowICellRendererParams<RowType extends GridBaseRow> extends ICellRendererParams {
   data: RowType;
 }
 
+export interface RowEditableCallbackParams<RowType extends GridBaseRow> extends EditableCallbackParams {
+  data: RowType;
+}
+
 export interface GenericCellColDef<RowType extends GridBaseRow> extends ColDefT<RowType> {
   cellRendererParams?: GenericCellRendererParams<RowType>;
+  editable?: boolean | ((params: RowEditableCallbackParams<RowType>) => boolean);
 }
 
 export interface GenericCellRendererParams<RowType extends GridBaseRow> {
