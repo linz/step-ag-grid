e4b65718b841b6aed588cff00a984d30dae3cc5c
Matt Dawson <mdawson@linz.govt.nz>
2023-02-08 10:23:48 +1300

    Multi select tests

--- src/stories/grid/gridFormInteraction/GridFormEditBearingInteraction.stories.tsx
+++ src/stories/grid/gridFormInteraction/GridFormEditBearingInteraction.stories.tsx
@@ -24,7 +24,7 @@ const Template: ComponentStory<typeof GridFormEditBearing> = (props) => {
   return (
     <div className={"react-menu-inline-test"}>
       <GridContextProvider>
-        <h6 ref={anchorRef}>Test</h6>
+        <h6 ref={anchorRef}>Interaction Test</h6>
         <GridPopoverContext.Provider
           value={
             {
