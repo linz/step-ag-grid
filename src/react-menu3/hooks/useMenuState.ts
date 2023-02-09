2ab751d072c4baeeaa583518bee4e19abfb1090f
matttdawson <89495499+matttdawson@users.noreply.github.com>
2023-02-09 14:25:50 +1300

    BREAKING CHANGE: Move aggrid peerdeps to deps (#207)
    Fix title cannot be null in aggrid 28
    
    BREAKING CHANGE: Move aggrid peerdeps to deps (#207)

--- src/react-menu3/hooks/useMenuState.ts
+++ src/react-menu3/hooks/useMenuState.ts
@@ -38,5 +38,5 @@ export const useMenuState = (
     exit: getTransition(transition, "close"),
   });
 
-  return [{ state: MenuStateMap[state], endTransition }, toggleMenu];
+  return [{ state: MenuStateMap[state.status], endTransition }, toggleMenu];
 };
