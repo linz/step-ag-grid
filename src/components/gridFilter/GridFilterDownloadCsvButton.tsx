7276faab9344b6bbeba546a9c57478f2ca6ec325
Matt Dawson <mdawson@linz.govt.nz>
2023-05-16 13:34:44 +1200

    Add spinner to download button

--- src/components/gridFilter/GridFilterDownloadCsvButton.tsx
+++ src/components/gridFilter/GridFilterDownloadCsvButton.tsx
@@ -1,16 +1,43 @@
 import { CsvExportParams } from "ag-grid-community/dist/lib/interfaces/exportParams";
-import { useContext } from "react";
+import { delay } from "lodash-es";
+import { useContext, useState } from "react";
+
+import { LuiMiniSpinner } from "@linzjs/lui";
 
 import { GridContext } from "../../contexts/GridContext";
+import { useStateDeferred } from "../../lui/stateDeferredHook";
 import { GridFilterHeaderIconButton } from "./GridFilterHeaderIconButton";
 
 export const GridFilterDownloadCsvButton = (csvExportParams: CsvExportParams) => {
   const { downloadCsv } = useContext(GridContext);
-  return (
+
+  const [downloading, setDownloading, setDownloadingDeferred] = useStateDeferred(false);
+
+  const handleDownloadClick = () => {
+    setDownloading(true);
+    // Defer the download such that the component disabled state can update
+    delay(() => {
+      downloadCsv(csvExportParams);
+      // Defer re-enablement as the browser takes time to process the download
+      setDownloadingDeferred(false, 4000);
+    }, 100);
+  };
+
+  return downloading ? (
+    <LuiMiniSpinner
+      size={22}
+      divProps={{
+        role: "status",
+        ["aria-label"]: "Downloading...",
+        style: { width: 42, display: "flex", justifyContent: "center" },
+      }}
+    />
+  ) : (
     <GridFilterHeaderIconButton
       icon={"ic_save_download"}
-      title={"Download CSV"}
-      onClick={() => downloadCsv(csvExportParams)}
+      title={downloading ? "Downloading..." : "Download CSV"}
+      onClick={handleDownloadClick}
+      disabled={downloading}
     />
   );
 };
