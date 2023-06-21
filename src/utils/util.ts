bd6a1c1f4a2920bce739cd420009e7c92ba3773e
Matt Dawson <mdawson@linz.govt.nz>
2023-06-22 10:59:58 +1200

    Tidy code

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -11,13 +11,10 @@ export const wait = (timeoutMs: number) =>
 const isFloatRegExp = /^-?\d*\.?\d*$/;
 export const isFloat = (value: string) => {
   try {
-    if (Number.isNaN(parseFloat(value))) {
-      return false;
-    }
-    // Just checking it's not scientific notation here.
+    // Just checking it's not scientific notation or "NaN" here.
     // Also parse float will parse up to the first invalid character,
     // so we need to check there's no remaining invalids e.g. "1.2xyz" would parse as 1.2
-    return isFloatRegExp.test(value);
+    return !Number.isNaN(parseFloat(value)) && isFloatRegExp.test(value);
   } catch {
     return false;
   }
