9c299c594dec7f615b41156b8fd9eb3850ec1beb
matttdawson <89495499+matttdawson@users.noreply.github.com>
2023-07-10 11:29:27 +1200

    fix: Remove sanitize filename due to use of buffer (#347)

--- src/utils/util.test.ts
+++ src/utils/util.test.ts
@@ -13,7 +13,17 @@ describe("sanitiseFileName", () => {
     expect(isFloat(".1")).toBe(true);
   });
   test("sanitiseFileName", () => {
-    expect(sanitiseFileName(" LT 1235/543 &%//*$ ")).toBe("LT_1235-543_&%-$");
-    expect(sanitiseFileName(" @filename here!!! ")).toBe("@filename_here!!!");
+    expect(sanitiseFileName(" LT 1235/543 &%//*$.csv ")).toBe("LT_1235-543_-.csv");
+    expect(sanitiseFileName(" @filename here!!!.csv ")).toBe("filename_here.csv");
+    // Trim to 64 char max for non extension part
+    expect(sanitiseFileName("long_long_long_long_long_long_long_long_long_long_long_long_long_long_.csv")).toBe(
+      "long_long_long_long_long_long_long_long_long_long_long_long_long.csv",
+    );
+    // Test with no extension
+    expect(sanitiseFileName("long_long_long_long_long_long_long_long_long_long_long_long_long_long_")).toBe(
+      "long_long_long_long_long_long_long_long_long_long_long_long_long",
+    );
+    // Test with no name
+    expect(sanitiseFileName(".csv")).toBe(".csv");
   });
 });
