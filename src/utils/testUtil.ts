4e33f4089a72cfdd77d7dfcb95883f24fcaeb9cc
matttdawson <89495499+matttdawson@users.noreply.github.com>
2023-08-16 14:42:09 +1200

    fix: wait for grid row Timeout is optional (#379)

--- src/utils/testUtil.ts
+++ src/utils/testUtil.ts
@@ -216,12 +216,12 @@ export const clickActionButton = async (text: string, container?: HTMLElement): 
   await user.click(button);
 };
 
-export const waitForGridReady = async (props?: { grid?: HTMLElement; timeout: number }) =>
+export const waitForGridReady = async (props?: { grid?: HTMLElement; timeout?: number }) =>
   waitFor(() => expect(getAllQuick({ classes: ".Grid-ready" }, props?.grid)).toBeInTheDocument(), {
     timeout: props?.timeout ?? 5000,
   });
 
-export const waitForGridRows = async (props?: { grid?: HTMLElement; timeout: number }) =>
+export const waitForGridRows = async (props?: { grid?: HTMLElement; timeout?: number }) =>
   waitFor(async () => expect(getAllQuick({ classes: ".ag-row" }, props?.grid).length > 0).toBe(true), {
     timeout: props?.timeout ?? 5000,
   });
