8a5b122aa345d1f9679c45460c09cbd41586dac7
matttdawson <89495499+matttdawson@users.noreply.github.com>
2024-07-02 11:32:11 +1200

    add search icon to left of quick search, placeholder is italic

--- src/components/gridFilter/GridFilterQuick.tsx
+++ src/components/gridFilter/GridFilterQuick.tsx
@@ -1,3 +1,4 @@
+import { LuiIcon } from "@linzjs/lui";
 import { useContext, useEffect, useState } from "react";
 
 import { GridContext } from "../../contexts/GridContext";
@@ -17,11 +18,14 @@ export const GridFilterQuick = ({ quickFilterPlaceholder, defaultValue }: GridFi
 
   return (
     <div className="GridFilterQuick-container">
+      <div className="GridFilterQuick-icon">
+        <LuiIcon name={"ic_search"} alt={"Search"} size={"md"} />
+      </div>
       <input
         aria-label="Search"
         className="GridFilterQuick-input"
         type="text"
-        placeholder={quickFilterPlaceholder ?? "Search..."}
+        placeholder={quickFilterPlaceholder ?? "Search"}
         value={quickFilterValue}
         onChange={(event) => {
           setQuickFilterValue(event.target.value);
