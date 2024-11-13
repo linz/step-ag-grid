92c50a1fbbb72aa6698eb34f2921860721c5ee54
Gabriel Liu <gabriel.liu3615@gmail.com>
2024-11-13 16:04:55 +1300

    text input format (#515)
    Co-authored-by: YLui <ylui@linz.govt.nz>

--- src/lui/FormError.tsx
+++ src/lui/FormError.tsx
@@ -1,6 +1,7 @@
 import "./FormError.scss";
 
 import { ReactElement } from "react";
+import { LuiIcon } from "@linzjs/lui";
 
 export interface FormErrorProps {
   helpText?: string;
@@ -11,9 +12,10 @@ export const FormError = (props: FormErrorProps) => {
   return (
     <>
       {props.error && (
-        <span className="LuiTextInput-error" style={{ paddingLeft: 0 }}>
-          {props.error}
-        </span>
+        <div className="FormError">
+          <LuiIcon alt="error" name="ic_error" className="FormError-text-icon" size="sm" status="error" />
+          <span className="FormError-error">{props.error}</span>
+        </div>
       )}
 
       {props.helpText && !props.error && <span className={"FormError-helpText"}>{props.helpText}</span>}
